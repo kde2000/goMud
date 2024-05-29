f5ded8bc242e0e8d3c7220f2a201ebf9f6fc48fe
kde2000 <kde2000@qq.com>
2024-05-29 08:18:53 +0800

    all 等需要修改

--- mudServer.go
+++ mudServer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strings"
 	"sync"
 )
 
@@ -15,11 +16,11 @@ type Scean struct {
 }
 
 type UserScean struct {
-	User  User
-	Scean Scean
+	User  *User
+	Scean *Scean
 }
 
-var GameWord = Scean{Location: "lobby", Billboard: make(chan string)}
+var GameWord = &Scean{Location: "lobby", Billboard: make(chan string)}
 var lock sync.RWMutex
 
 // mud服务器，带玩家和场景
@@ -61,7 +62,7 @@ func (me *MudServer) Start() {
 		}
 		//登记用户
 		fmt.Println("server accepting...", conn.RemoteAddr().String())
-		newbie := *NewUser(conn.RemoteAddr().String(), conn, me)
+		newbie := NewUser(conn.RemoteAddr().String(), conn, me)
 		lock.Lock()
 		me.Family[conn.RemoteAddr().String()] = UserScean{User: newbie, Scean: GameWord}
 		lock.Unlock()
@@ -73,6 +74,8 @@ func (me *MudServer) Start() {
 func (me *MudServer) Handle(conn net.Conn) {
 	fmt.Println("User in:", me.Family)
 	defer conn.Close()
+	//很重要。匿名函数可以使用主函数的变量
+	//一个单独的协程来处理读入数据
 	go func() {
 		var buff = make([]byte, 4096)
 		for {
@@ -82,11 +85,32 @@ func (me *MudServer) Handle(conn net.Conn) {
 				return
 			}
 			if n > 0 {
-				me.BroadCast(string(buff[:n-1]))
+				inString := strings.Split(string(buff[:n-1]), "|")
+				comd := strings.Trim(inString[0], " ")
+				fmt.Println("comd in:", comd)
+				switch comd {
+				case "all":
+					me.BroadCast(string(buff[:n-1]))
+				case "who", "WHO":
+					me.Who()
+				case "exit", "EXIT":
+					me.BroadCast(conn.RemoteAddr().String() + " offline...")
+					return
+				default:
+					if len(inString) > 1 {
+						msgtxt := inString[1]
+						me.SendUserMessage(comd, msgtxt)
+					} else {
+						me.SendUserMessage(conn.RemoteAddr().String(), "格式问题：用户名|信息。命令：all，who，exit")
+					}
+
+				}
+
 			}
 		}
 
 	}()
+	//很重要。机制不清楚。阻塞主协程
 	select {}
 
 }
@@ -94,6 +118,23 @@ func (me *MudServer) BroadCast(msg string) {
 	lock.Lock()
 	for _, v := range me.Family {
 		v.User.Ch <- msg
+	}
+	lock.Unlock()
+}
+func (me *MudServer) Who() {
+	lock.Lock()
+	for _, v := range me.Family {
+		me.BroadCast(v.User.Name)
+	}
+	lock.Unlock()
+}
+func (me *MudServer) SendUserMessage(username, msg string) {
+	lock.Lock()
+	for _, v := range me.Family {
+		if v.User.Name == username {
+			v.User.Ch <- msg
+		}
+
 	}
 	lock.Unlock()
 }
